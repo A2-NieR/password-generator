ed20c3cd99a31dfde84d33d0a240a66656add0d9
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-08-24 12:10:41 +0200

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Password Generator</h1>
-  <generator/>
+  <generator />
 </template>
 
 <script>
@@ -20,7 +20,30 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin-top: 15%;
+}
+
+body {
+  background: linear-gradient(
+    -45deg,
+    #2E3192,
+    #1BFFFF
+  );
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 100vh;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="container">
     <form @submit.prevent="submitParams">
-      <input type="text" v-model="password" />
+      <input type="text" v-model="password" id="final-pw" />
       <div class="includes">
-        <label for="checkbox">A-Z</label>
-        <input
-          type="checkbox"
-          v-model="includeUppercase"
-        />
-        <label for="checkbox">a-z</label>
-        <input
-          type="checkbox"
-          v-model="includeLowercase"
-        />
-        <label for="checkbox">0-9</label>
-        <input
-          type="checkbox"
-          v-model="includeNumbers"
-        />
-        <label for="checkbox">!@#$%^&*</label>
-        <input
-          type="checkbox"
-          v-model="includeSpecial"
-        />
+        <label class="checkbox">
+          <span>A-Z</span>
+          <input type="checkbox" v-model="includeUppercase" />
+        </label>
+        <label class="checkbox">
+          <span>a-z</span>
+          <input type="checkbox" v-model="includeLowercase" />
+        </label>
+        <label class="checkbox">
+          <span>0-9</span>
+          <input type="checkbox" v-model="includeNumbers" />
+        </label>
+        <label class="checkbox">
+          <span>Symbols</span>
+          <input type="checkbox" v-model="includeSpecial" />
+        </label>
       </div>
       <div class="length">
         <label>Password Length: </label>
-        <input type="number" v-model.number="passwordLength" />
+        <input type="number" v-model.number="passwordLength" id="pw-length" />
       </div>
       <button>Generate</button>
     </form>
@@ -70,4 +66,96 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#final-pw {
+  height: 30px;
+  width: 220px;
+  border-radius: 4px;
+  border: none;
+  margin: 15px;
+  text-align: center;
+  font-size: 1rem;
+  color: #2E3192;
+}
+
+.includes {
+  margin: 15px;
+}
+
+input[type="checkbox"] {
+  color: teal;
+}
+
+.length {
+  margin: 15px;
+}
+
+#pw-length {
+  height: 30px;
+  width: 50px;
+  border-radius: 4px;
+  border: none;
+  text-align: center;
+  font-size: 1rem;
+  color: #2E3192;
+}
+
+button {
+  height: 40px;
+  width: 100px;
+  background: darkorange;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  margin: 15px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+}
+
+label {
+  margin-left: 15px;
+}
+
+.checkbox {
+  display: inline-flex;
+  cursor: pointer;
+  position: relative;
+  align-items: center;
+}
+
+.checkbox > span {
+  padding: 0.5rem 0.25rem;
+}
+
+.checkbox > input {
+  height: 1.5em;
+  width: 1.5em;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -o-appearance: none;
+  appearance: none;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  transition-duration: 0.3s;
+  background-color: white;
+  cursor: pointer;
+}
+
+.checkbox > input:checked {
+  background-color: darkorange;
+}
+
+.checkbox > input:checked + span::before {
+  display: block;
+  text-align: center;
+  color: darkorange;
+  position: absolute;
+  left: 0.7rem;
+  top: 0.2rem;
+}
+
+.checkbox > input:active {
+  border: 2px solid darkorange;
+}
+</style>
